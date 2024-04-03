36c009991082dc37cc3c0bd3a580997c76662266
Victor Godwin <124065747+vikzenith@users.noreply.github.com>
2024-04-03 14:31:36 +0100

    Commit 1

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -1,17 +1,24 @@
 #!/usr/bin/env python3
-""" Basic dictionary
+""" FIFO caching
 """
 BaseCaching = __import__('base_caching').BaseCaching
 
 
-class BasicCache(BaseCaching):
-    """ BasicCache
+class FIFOCache(BaseCaching):
+    """ FIFOCache
     """
+    def __init__(self):
+        super().__init__()
+
     def put(self, key, item):
         """ assigns the new item to the dictionary
         """
         if not (key is None or item is None):
-            self.cache_data[key] = item
+            self.cache_data.update({key: item})
+            if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+                first_in = list(self.cache_data.keys())[0]
+                del self.cache_data[first_in]
+                print(f'DISCARD: {first_in}')
 
     def get(self, key):
         """ returns the value in self.cache_data linked to key
